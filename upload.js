6235ae2bf0c7a3c1581a2e0bd6e7c46c44ecfa9e
Goat <gzhseo@163.com>
2016-08-22 21:29:43 +0800

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,9 +1,41 @@
-;(function(w) {
+;
+(function(w) {
     "use strict"
-    var upload = {};
+    //通过id获取元素的方法
+    var $$ = function(id) {
+        return document.getElementById(id);
+    };
 
-    upload.create = function(config){
-        
+
+    var upload = {};
+    //创建上传插件实例
+    upload.create = function(config) {
+        var inputId = config.id;
+        var url = config.url;
+
+        var fileInput = $$(inputId);
+
+        fileInput.onchange = function(e) {
+            e.preventDefault();
+            var reader = new FileReader();
+            reader.readAsBinaryString(fileInput.files[0]);
+            reader.onloadend = function() {
+                upload.ajax(reader.result, url);
+            };
+        };
+    };
+    //异步上传的方法
+    upload.ajax = function(data, url) {
+        var xhr = new XMLHttpRequest(),
+            method = "GET";
+
+        xhr.open(method, url, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                console.log(xhr.responseText);
+            }
+        };
+        xhr.sendAsBinary(data);
     };
 
     w.upload = upload;
